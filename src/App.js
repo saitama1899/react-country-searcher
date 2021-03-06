a1609208779cf51c56e34a17d174387322ba32d9
Eric <eric.selva@principiosactivos.com>
2021-03-06 11:13:41 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
 
+import {useEffect, useState} from 'react'
+import axios from 'axios'
+
+import CountryList from './Components/CountryList'
+import Country from './Components/Country'
+import SearchCountry from './Components/SearchCountry'
+
+require('dotenv').config()
+
 function App() {
+
+  const [countries, setCountries] = useState([])
+  const [newSearch, setnewSearch] = useState('')
+  const [tooMuchResults, settooMuchResults] = useState(false)
+
+  const checkLimit = (countries) => {
+    if (countries.length <= 10) {
+      setCountries(countries)
+      settooMuchResults(false)
+    } else {
+      setCountries([])
+      settooMuchResults(true)
+    }
+  }
+
+  useEffect(() => {
+    if(newSearch) {
+      axios
+      .get(`https://restcountries.eu/rest/v2/name/${newSearch}`)
+      .then(response => {
+        const {data} = response
+        const c = 
+          data.filter(country => country.name.toLowerCase()
+            .includes(newSearch.toLowerCase()))
+
+        checkLimit(c)
+      })
+    } else {
+      settooMuchResults(false) // Para evitar el mensaje residual
+    }
+  }, [newSearch])
+
+  const handleChange = (e) => {
+    setnewSearch(e.target.value)
+  }
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='card'>
+      <h1>Country search</h1>
+
+      <SearchCountry onChange={handleChange} value={newSearch}/>
+
+      {tooMuchResults ? <h3>Too much results to show</h3> :
+        countries.length === 1 ?
+          <Country country={countries[0]}/> : <CountryList countries={countries} />
+      }
     </div>
   );
 }
